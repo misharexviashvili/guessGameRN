efb18098c69ca1fea1b53004129532024871ae96
Misha <misharexviashvili@yahoo.com>
2023-01-29 00:45:44 +0400

    log data output

---
+++ components/game/GuessLogItem.js
@@ -0,0 +1,33 @@
+import { Text, View, StyleSheet } from "react-native";
+import Colors from "../../constants/colors";
+
+export default function GuessLogItem({ roundNumber, guess }) {
+  return (
+    <View style={styles.listItem}>
+      <Text>#{roundNumber}</Text>
+      <Text style={styles.itemText}>Opponent's Guess: {guess}</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  listItem: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    borderColor: Colors.primary800,
+    borderWidth: 1,
+    borderRadius: 40,
+    padding: 12,
+    marginVertical: 8,
+    backgroundColor: Colors.accent500,
+    width: "100%",
+    elevation: 4,
+    shadowColor: "black",
+    shadowOffset: { width: 0, height: 0 },
+    shadowOpacity: 0.25,
+    shadowRadius: 3,
+  },
+  itemText: {
+    fontFamily: "Open-sans",
+  },
+});
--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -6,6 +6,7 @@ import NumberContainer from "../components/game/NumberContainer";
 import PrimaryButton from "../components/ui/PrimaryButton";
 import Card from "../components/ui/Card";
 import InstructionText from "../components/ui/InstructionText";
+import GuessLogItem from "../components/game/GuessLogItem";
 
 function generateRandomBetween(min, max, exclude) {
   const rndNum = Math.floor(Math.random() * (max - min)) + min;
@@ -53,6 +54,7 @@ function GameScreen({ userNumber, onGameOver }) {
     setCurrentGuess(newRndNumber);
     setGuessRounds((prevState) => [newRndNumber, ...prevState]);
   }
+  const guessRoundsListLength = guessRounds.length;
   return (
     <View style={styles.screen}>
       <Title>Opponent's guess</Title>
@@ -75,12 +77,16 @@ function GameScreen({ userNumber, onGameOver }) {
         </View>
       </Card>
       <View>
-        {/* {guessRounds.map((guessRound) => (
-          <Text key={guessRound}>{guessRound}</Text>
-        ))} */}
         <FlatList
           data={guessRounds}
-          renderItem={(itemData) => <Text>{itemData.item}</Text>}
+          renderItem={(itemData) => {
+            return (
+              <GuessLogItem
+                roundNumber={guessRoundsListLength - itemData.index}
+                guess={itemData.item}
+              />
+            );
+          }}
           keyExtractor={(item) => item}
         />
       </View>
