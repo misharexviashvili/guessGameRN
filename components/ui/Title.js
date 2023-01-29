5402ae58995f02ead65c084c50a74cd789a9a330
Misha <misharexviashvili@yahoo.com>
2023-01-29 14:52:50 +0400

    title height responsiveness for landscape mode

--- components/ui/Title.js
+++ components/ui/Title.js
@@ -1,6 +1,13 @@
-import { Text, StyleSheet } from "react-native";
+import { Text, StyleSheet, useWindowDimensions } from "react-native";
 function Title({ children }) {
-  return <Text style={styles.title}>{children}</Text>;
+  const { width, height } = useWindowDimensions();
+  let marginTop = 50;
+  if (height < 400) {
+    marginTop = 15;
+  }
+  return (
+    <Text style={[styles.title, { marginTop }]}>{children}</Text>
+  );
 }
 export default Title;
 
@@ -13,6 +20,7 @@ const styles = StyleSheet.create({
     textAlign: "center",
     borderWidth: 2,
     borderColor: "white",
+    // marginTop:50,
     padding: 12,
     maxWidth: "80%",
     width: 300,
